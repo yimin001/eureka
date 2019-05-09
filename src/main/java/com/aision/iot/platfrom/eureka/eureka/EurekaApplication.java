3fa420fd4d232b71907359014d000bb3e9f0a496
yimin001 <540019320@qq.com>
2019-05-09 13:50:19 +0800

    first commit

---
+++ src/main/java/com/aision/iot/platfrom/eureka/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.aision.iot.platfrom.eureka.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yim
+ * @description 启动类
+ * @date 2019/4/26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
